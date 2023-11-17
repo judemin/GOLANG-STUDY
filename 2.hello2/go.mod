d7343928a0dee92ce5bca55ea2ea2cf93484c1b8
judemin <judemin2087@naver.com>
2023-11-17 17:44:33 +0900

    ADD : add all project

---
+++ 2.hello2/go.mod
@@ -0,0 +1,3 @@
+module GOLANG-STUDY/CH1/hello2
+
+go 1.21.4
---
+++ 4-3/4-3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int
+	var c = 4
+	d := 12
+	var e = "Hello"
+	f := 3.14
+
+	fmt.Println(a, b, c, d, e, f)
+}
